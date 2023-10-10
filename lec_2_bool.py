065fa634326628056df7e600eb7d38d9f9c344bd
fiyfiycugcoid <146106434+fiyfiycugcoid@users.noreply.github.com>
2023-10-10 15:53:43 +0000

    bfsb

---
+++ lec_2_bool.py
@@ -0,0 +1,13 @@
+print(bool(2))
+
+print(bool('good'))
+
+print(bool([1,3,4]))
+
+print(bool(0))
+
+print(bool(''))
+
+print(bool([]))
+
+print(bool([[]]))
---
+++ lec_2_if-elif-else.py
@@ -0,0 +1,8 @@
+a=3
+if a > 5:
+    print('hello5')
+elif a < 2:
+    print(' hello2')
+
+else:
+    print('tupo hello')
